af6d3332f032a490a190ee194c00be767b01e9ab
BrianOfrim <bofrim@ualberta.ca>
2020-05-05 20:56:25 -0700

    refactored and imporved pytorch overlay example

---
+++ examples/detection.py
@@ -0,0 +1,185 @@
+import fnmatch
+import os
+import svgwrite
+
+import numpy as np
+import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import torchvision.transforms.functional as F
+
+from overlay import run_pipeline
+
+
+def find_file(name, path) -> str:
+    for root, dirs, files in os.walk(path):
+        filematch = next((f for f in files if fnmatch.fnmatch(f, name)), None)
+        if filematch:
+            return os.path.join(root, filematch)
+    return None
+
+
+def get_model(num_classes, **kwargs):
+    # load a model pre-trained pre-trained on COCO
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+        pretrained=True, **kwargs
+    )
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
+
+
+def draw_text(dwg, x, y, text, font_size=25):
+    dwg.add(
+        dwg.text(text, insert=(x, max(y, font_size)), fill="white", font_size=font_size)
+    )
+
+
+def draw_rect(dwg, x, y, w, h, stroke_color="red", stroke_width=4):
+    dwg.add(
+        dwg.rect(
+            insert=(x, y),
+            size=(w, h),
+            fill="none",
+            stroke=stroke_color,
+            stroke_width=stroke_width,
+        )
+    )
+
+
+def main(args):
+
+    model_path = args.model_path
+
+    if model_path is not None:
+        if not os.path.isfile(model_path):
+            print(f"Error: Could not locate model file: {model_path}")
+            return
+    else:
+        model_path = find_file("*.pt", os.getcwd())
+        if model_path is None:
+            print(f"Error: Could not locate a model '.pt' file in {os.getcwd()}")
+            return
+
+    label_path = args.label_path
+
+    if label_path is not None:
+        if not os.path.isfile(label_path):
+            print(f"Error: Could not locate label file: {label_path}")
+            return
+    else:
+        label_path = find_file("labels.txt", os.getcwd())
+        if label_path is None:
+            print(f"Error: Could not find label text file at: {label_path}")
+            return
+    labels = open(label_path).read().splitlines()
+
+    if len(labels) == 0:
+        print("Error: No labels found in file")
+
+    # Add the background as the first class
+    labels.insert(0, "background")
+
+    print("Labels:")
+    print(labels)
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    model = get_model(
+        len(labels),
+        box_score_thresh=args.threshold,
+        min_size=600,
+        max_size=800,
+        box_nms_thresh=0.3,
+    )
+
+    print("Loading model state from: %s" % model_path)
+    checkpoint = torch.load(model_path, map_location=device)
+
+    print(checkpoint.keys())
+
+    model.load_state_dict(checkpoint["model"])
+
+    # move model to the right device
+    model.to(device)
+
+    model.eval()
+
+    def user_callback(image_data):
+
+        with torch.no_grad():
+
+            if image_data is None:
+                return None
+
+            tensor_image = F.to_tensor(image_data)
+            tensor_image = tensor_image.to(device)
+
+            outputs = model([tensor_image])
+            outputs = [
+                {k: v.to(torch.device("cpu")) for k, v in t.items()} for t in outputs
+            ]
+
+            # filter out the background labels and scores bellow threshold
+            filtered_outputs = [
+                (
+                    outputs[0]["boxes"][j],
+                    outputs[0]["labels"][j],
+                    outputs[0]["scores"][j],
+                )
+                for j in range(len(outputs[0]["boxes"]))
+                if outputs[0]["scores"][j] > args.threshold
+                and outputs[0]["labels"][j] > 0
+            ]
+
+            dwg = svgwrite.Drawing("", size=(image_data.shape[1], image_data.shape[0]))
+
+            for detection in filtered_outputs:
+                bbox = detection[0].tolist()
+                label_index = int(detection[1])
+                score = float(detection[2])
+                x, y = bbox[0], bbox[1]
+                w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
+
+                draw_text(dwg, x, y - 5, f"{labels[label_index]} [{(100*score):.2f}%]")
+                draw_rect(dwg, x, y, w, h, stroke_color="red", stroke_width=4)
+
+            return dwg.tostring()
+
+    run_pipeline(
+        user_callback,
+        src_frame_rate=args.frame_rate,
+        src_height=args.source_height,
+        src_width=args.source_width,
+    )
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model_path", type=str, help="The model to load")
+
+    parser.add_argument("--label_path", type=str, help="The labels to use")
+
+    parser.add_argument("--source_width", type=int)
+
+    parser.add_argument("--source_height", type=int)
+
+    parser.add_argument("--frame_rate", type=float)
+
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        default=0.5,
+        help="The threshold above which to display predicted bounding boxes",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ examples/overlay.py
@@ -0,0 +1,143 @@
+# Based on https://github.com/google-coral/examples-camera/tree/master/gstreamer
+import sys
+import svgwrite
+import threading
+
+import numpy as np
+
+import gi
+
+gi.require_version("Gst", "1.0")
+gi.require_version("GstBase", "1.0")
+gi.require_version("Gtk", "3.0")
+from gi.repository import GLib, GObject, Gst, GstBase, Gtk
+
+GObject.threads_init()
+Gst.init(None)
+
+
+class GstPipeline:
+    def __init__(self, pipeline, user_function):
+        self.user_function = user_function
+        self.running = False
+        self.gstbuffer = None
+        self.sink_size = None
+        self.box = None
+        self.condition = threading.Condition()
+
+        self.pipeline = Gst.parse_launch(pipeline)
+        self.overlay = self.pipeline.get_by_name("overlay")
+        appsink = self.pipeline.get_by_name("appsink")
+        appsink.connect("new-sample", self.on_new_sample)
+
+        # Set up a pipeline bus watch to catch errors.
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message", self.on_bus_message)
+
+    def run(self):
+        # Start vision worker.
+        self.running = True
+        worker = threading.Thread(target=self.vision_loop)
+        worker.start()
+
+        # Run pipeline.
+        self.pipeline.set_state(Gst.State.PLAYING)
+        try:
+            Gtk.main()
+        except:
+            pass
+
+        # Clean up.
+        self.pipeline.set_state(Gst.State.NULL)
+        while GLib.MainContext.default().iteration(False):
+            pass
+        with self.condition:
+            self.running = False
+            self.condition.notify_all()
+        worker.join()
+
+    def on_bus_message(self, bus, message):
+        t = message.type
+        if t == Gst.MessageType.EOS:
+            Gtk.main_quit()
+        elif t == Gst.MessageType.WARNING:
+            err, debug = message.parse_warning()
+            sys.stderr.write("Warning: %s: %s\n" % (err, debug))
+        elif t == Gst.MessageType.ERROR:
+            err, debug = message.parse_error()
+            sys.stderr.write("Error: %s: %s\n" % (err, debug))
+            Gtk.main_quit()
+        return True
+
+    def on_new_sample(self, sink):
+        sample = sink.emit("pull-sample")
+        if not self.sink_size:
+            s = sample.get_caps().get_structure(0)
+            self.sink_size = (s.get_value("height"), s.get_value("width"))
+        with self.condition:
+            self.gstbuffer = sample.get_buffer()
+            self.condition.notify_all()
+        return Gst.FlowReturn.OK
+
+    def vision_loop(self):
+        local_np_buffer = None
+
+        while True:
+            with self.condition:
+                while not self.gstbuffer and self.running:
+                    self.condition.wait()
+                if not self.running:
+                    break
+                local_gst_buffer = self.gstbuffer
+                self.gstbuffer = None
+
+            if local_np_buffer is None:
+                local_np_buffer = np.zeros((*self.sink_size, 3), dtype=np.uint8)
+
+            result, mapinfo = local_gst_buffer.map(Gst.MapFlags.READ)
+            if result:
+                local_np_buffer[:] = np.ndarray(
+                    (*self.sink_size, 3), buffer=mapinfo.data, dtype=np.uint8
+                )
+                local_gst_buffer.unmap(mapinfo)
+
+                svg = self.user_function(local_np_buffer)
+                if svg and self.overlay:
+                    self.overlay.set_property("data", svg)
+
+
+def run_pipeline(
+    user_function,
+    src_frame_rate: float = None,
+    src_height: int = None,
+    src_width: int = None,
+):
+
+    image_src_element = "pyspinsrc"
+    image_src_caps = "video/x-raw,format=RGB"
+    if src_frame_rate is not None:
+        image_src_caps += f",framerate={src_frame_rate}"
+
+    if src_height is not None:
+        image_src_caps += f",height={src_height}"
+
+    if src_width is not None:
+        image_src_caps += f",width={src_width}"
+
+    appsink_element = "appsink name=appsink emit-signals=true max-buffers=1 drop=true"
+    appsink_caps = "video/x-raw,format=RGB"
+    leaky_queue = "queue max-size-buffers=1 leaky=downstream"
+
+    image_display_element = "ximagesink sync=false"
+    overlay_element = "rsvgoverlay name=overlay"
+
+    pipeline = f""" {image_src_element} ! {image_src_caps} ! tee name=t
+        t. ! {leaky_queue} ! videoconvert ! {appsink_caps} ! {appsink_element}
+        t. ! {leaky_queue} ! videoconvert ! {overlay_element} ! videoconvert ! {image_display_element}
+        """
+
+    print("Gstreamer pipeline:\n", pipeline)
+
+    pipeline = GstPipeline(pipeline, user_function)
+    pipeline.run()
